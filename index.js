27b09bf102d4799f4e547de97105b5a4add6a18c
nohyp3 <84885039+nohyp3@users.noreply.github.com>
2021-06-30 19:53:19 -0400

    basic website layout

---
+++ index.js
@@ -0,0 +1,78 @@
+var webProj= [
+    {
+      name: "Project 1",
+     img: "https://media.istockphoto.com/vectors/geometric-banner-megaphone-with-coming-soon-bubble-loudspeaker-modern-vector-id1181378326?k=6&m=1181378326&s=612x612&w=0&h=G9QQjvLpHdFmKtHkaRbt08NnGgiDbtxZyR5L629Bck0=",
+     description: "This was my first project!"
+    },
+    {
+      name: "Project 2",
+      img: "https://media.istockphoto.com/vectors/geometric-banner-megaphone-with-coming-soon-bubble-loudspeaker-modern-vector-id1181378326?k=6&m=1181378326&s=612x612&w=0&h=G9QQjvLpHdFmKtHkaRbt08NnGgiDbtxZyR5L629Bck0=",
+      description: "This was my second project!" 
+    }];
+  const name = document.getElementById("name");
+  const img = document.getElementById("img");
+  const description = document.getElementById("description");
+  let currentCard = 0;
+  function showProject(project) {
+    const item = webProj[project]
+    name.textContent = item.name;
+    img.src = item.img;
+    description.textContent = item.description;
+  }
+  
+  nextBtn.addEventListener('click', function() {
+    currentCard++;
+    if(currentCard > webProj.length - 1){
+      currentCard = 0;
+    }
+    showProject(currentCard);
+  })
+  
+  prevBtn.addEventListener('click', function() {
+    currentCard--;
+    if(currentCard < 0) {
+      currentCard = webProj.length -1;
+    }
+    showProject(currentCard);
+  })
+  //graphic design carousel 
+  var graphicProj= [
+    {
+      name: "Project 1",
+     img: "https://media.istockphoto.com/vectors/geometric-banner-megaphone-with-coming-soon-bubble-loudspeaker-modern-vector-id1181378326?k=6&m=1181378326&s=612x612&w=0&h=G9QQjvLpHdFmKtHkaRbt08NnGgiDbtxZyR5L629Bck0=",
+     description: "This was my first project!"
+    },
+    {
+      name: "Project 2",
+      img: "https://media.istockphoto.com/vectors/geometric-banner-megaphone-with-coming-soon-bubble-loudspeaker-modern-vector-id1181378326?k=6&m=1181378326&s=612x612&w=0&h=G9QQjvLpHdFmKtHkaRbt08NnGgiDbtxZyR5L629Bck0=",
+      description: "This was my second project!" 
+    }];
+  const nameG = document.getElementById("nameG");
+  const imgG = document.getElementById("imgG");
+  const descriptionG = document.getElementById("descriptionG");
+  let currentCardG = 0;
+  function showProjectG(project) {
+    const itemG = graphicProj[project]
+    nameG.textContent = itemG.name;
+    imgG.src = itemG.img;
+    descriptionG.textContent = itemG.description;
+  }
+  
+  nextBtnG.addEventListener('click', function() {
+    currentCardG++;
+    if(currentCardG > graphicProj.length - 1){
+      currentCardG = 0;
+    }
+    showProjectG(currentCardG);
+  })
+  
+  prevBtnG.addEventListener('click', function() {
+    currentCardG--;
+    if(currentCardG < 0) {
+      currentCardG = graphicProj.length -1;
+    }
+    showProjectG(currentCardG);
+  })
+  
+  
+  
